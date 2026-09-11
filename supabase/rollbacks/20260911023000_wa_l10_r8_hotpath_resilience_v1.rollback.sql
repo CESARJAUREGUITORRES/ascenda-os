649fc6179a0ecfb25679769f743b8a2c93cb8916
CESARJAUREGUITORRES <jaureguitorrescesar@gmail.com>
2026-09-10 21:22:45 -0500

    WA-L10 R8: add hotpath resilience rollback

---
+++ supabase/rollbacks/20260911023000_wa_l10_r8_hotpath_resilience_v1.rollback.sql
@@ -0,0 +1,3 @@
+-- WA-L10 R8 hotpath resilience rollback.
+drop function if exists public.aos_wa4_toxin_price_fast_v1();
+drop function if exists public.aos_wa_l10_internal_canary_authorize_v1(uuid);
